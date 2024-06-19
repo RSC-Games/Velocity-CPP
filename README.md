95ebf84a6dd7e8dd75a6ed90f89785ffa7bb0105
michabay05 <michabay05@gmail.com>
2024-06-19 18:40:49 -0400

    Added cmake library linking

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Velocity-CPP
 
 An industry-grade game engine written for C++. The base engine files will be hosted here, with extension modules that can optionally be linked in.
+
+## Quickstart
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,65 @@
-#include <iostream>
+// DUMMY GLFW PROGRAM
 
-int main(void) {
-    std::cout << "Hello, world!" << std::endl;
+#define GLAD_GL_IMPLEMENTATION
+#include "glad/glad.h"
+#define GLFW_INCLUDE_NONE
+#include <glfw-linux/glfw3.h>
 
-    return 0;
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main(void)
+{
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return 1;
+    }
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
